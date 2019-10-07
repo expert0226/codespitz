95245599fa307673507e5137e0ddd6e5a9297bb8
James <james@cizion.com>
2019-10-07 21:55:01 +0900

    day03 강의 내용 정리

--- src/main/resources/codespitz79/designpattern/day03/todo01.js
+++ src/main/resources/codespitz79/designpattern/day03/todo01.js
@@ -1,16 +1,5 @@
-const Sort = class {
-    static title = (a, b) => a.sortTitle(b);
-    static date = (a, b) => a.sortDate(b);
-
-    sortTitle(task) { throw "override"; }
-    sortDate(task) { throw "override"; }
-};
-
-const Task = class extends Sort {
-    static get(title, date = null) { return new Task(title, date); }
-
-    constructor(title, date) {
-        super();
+const Task = class {
+    constructor(title, date = null) {
         if(!title) throw "invalid title";
         this._title = title;
         this._date = date;
@@ -19,59 +8,11 @@ const Task = class extends Sort {
 
     isComplete() { return this._isComplete; }
     toggle() { this._isComplete = !this._isComplete; }
-    sortTitle(task) { return this._title > task._title ? 1 : -1; }
-    sortDate(task) { return this._date > task._date ? 1 : -1; }
+    sortTitle(task) { return this._title > task._title; }
+    sortDate(task) { return this._date > task._date; }
 };
 
-const TaskList = class {
-    constructor(title) {
-        if(!title) throw "invalid title";
-        this._title = title;
-        this._list = [];
-    }
-
-    add(title, date) { this._list.push(Task.get(title, date)); }
-    remove(task) {
-        const list = this._list;
-        if(list.includes(task)) list.splice(list.indexOf(task), 1);
-    }
-
-    byTitle(stateGroup = true) { return this._getList(Sort.title, stateGroup); }
-    byDate(stateGroup = true) { return this._getList(Sort.date, stateGroup); }
-    _getList(sort, stateGroup) {
-        const list = this._list;
-        return !stateGroup ? list.sort(sort) : [
-            ...list.filter(v => !v.isComplete()).sort(sort),
-            ...list.filter(v => v.isComplete()).sort(sort)
-        ]
-    }
+const taskSort = {
+    title: (a, b) => a.sortTitle(b),
+    date: (a, b) => b.sortDate(b)
 };
-
-// User Story
-{
-    const list1 = new TaskList("비사이드");
-
-    list1.add("지라 설치");
-    list1.add("지라 클라우드 접속");
-
-    const list2 = new TaskList("s3-4");
-    list2.add("2강 답안 작성", "2019-09-01");
-    list2.add("3강 교안 작성", "2019-10-01");
-
-    console.log(list1.byTitle());
-    console.log(list2.byDate());
-}
-
-{
-    const list1 = new TaskList("비사이드");
-
-    list1.add("지라 클라우드 접속");
-    list1.add("지라 설치");
-
-    const list2 = new TaskList("s3-4");
-    list2.add("2강 답안 작성", "2019-11-30");
-    list2.add("3강 교안 작성", "2019-10-31");
-
-    console.log(list1.byTitle());
-    console.log(list2.byDate());
-}
---
+++ src/main/resources/codespitz79/designpattern/day03/todo04.js
@@ -0,0 +1,89 @@
+const Task = class {
+    constructor(title, date) {
+        this._title = title;
+        this._date = date;
+        this._isComplte = false;
+        this._list = [];
+    }
+
+    isComplete() { return this._isComplete; }
+    toggle() { this._isComplete = !this._isComplete; }
+
+    add(title, date = null) { this._list.push(new Task(title, date)); }
+    remove(task) {
+        const list = this._list;
+        if(list.includes(task)) list.splice(list.indexOf(task), 1);
+    }
+
+    byTitle(useGroupSort = true) { return this.list("title", useGroupSort); }
+    byDate(useGroupSort = true) { return this.list("date", useGroupSort); }
+
+    list(sort, useGroupSort = true) {
+        const list = this._list;
+        const comparator = (a, b) => a['_' + sort] > b['_' + sort] ? 1 : -1;
+        const mapper = task => task.list(sort, useGroupSort);
+        return {
+            task: this,
+            list: !useGroupSort ? list.sort(comparator).map(mapper) : [
+                ...list.filter(v => !v.isComplete()).sort(comparator).map(mapper),
+                ...list.filter(v => v.isComplete()).sort(comparator).map(mapper)
+            ]
+        }
+    }
+};
+
+const el = (tag, attr = {}) => Object.entries(attr).reduce((el, v) => {
+    typeof el[v[0]] == "function" ? el[v[0]](v[1]) : el[v[0]] = v[1];
+    return el;
+}, document.createElement(tag));
+
+const DomRenderer = class {
+    constructor(parent) {
+        this._parent = parent;
+    }
+
+    render(data) {
+        const { task: { _title: title }, list } = data;
+        const parent = document.querySelector(this._parent);
+        parent.innerHTML = "";
+        parent.appendChild(el("h1", { innerHTML: title }));
+        parent.appendChild(this._render(el("ul"), list));
+    }
+
+    _render(parent, list) {
+        list.forEach(({ task, list }) => {
+            const checkbox = el("input", { type: "checkbox", checked: task.isComplete() });
+            const div = el("div", { innerHTML: task._title });
+            div.appendChild(checkbox);
+
+            const li = parent.appendChild(el("li"));
+            li.appendChild(div);
+            if(list.length) li.appendChild(this._render(el("ul"), list));
+        });
+        return parent;
+    }
+};
+
+// User Story
+{
+    const folder = new Task("s3-4");
+
+    folder.add("2강 교안 작성");
+    folder.add("3강강 교안 작성");
+
+    const { list } = folder.list("title");
+
+    list[1].task.add("ppt 정리");
+    list[1].task.add("코드 정리");
+
+    const { list: sublist } = list[1].task.list("title");
+
+    sublist[1].task.add("슬라이드 마스터 정리");
+    sublist[1].task.add("디자인 개선");
+    sublist[1].task._isComplete = true;
+
+    const todo = new DomRenderer("#a");
+
+    todo.render(folder.list("title"));
+    console.log(folder.list("title"))
+}
